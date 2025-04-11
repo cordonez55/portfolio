430709e74706eba830950c1a0e9c2fa7c8a0f357
cordonez55 <cassy.ordonez@gmail.com>
2025-04-10 22:36:16 -0500

    Final content for all sectiosn completed before styling and Docker setup

--- src/pages/Setup.jsx
+++ src/pages/Setup.jsx
@@ -1,5 +1,29 @@
 function Setup () {
-  return <h2>Setup Page</h2>;
+  return (
+    <div>
+      <h2>Developer Setup</h2>
+
+      <section>
+        <h3>Code Editor</h3>
+        <p>I primarily use <strong>VS Code</strong> as my editor. I like its extensions, built-in terminal, and how it integrates well with React and GitHub.</p>
+      </section>
+
+      <section>
+        <h3>Terminal & CLI</h3>
+        <p>I'm still learning how to use the command line more efficiently. For now, I mostly use <strong>PowerShell</strong> and the integrated terminal in VS Code. I do look up online resources for commands I'm not familiar with, but I'm getting better through practice.</p>
+      </section>
+
+      <section>
+        <h3>Editor Font</h3>
+        <p>I currently use the default font in VS Code, but I like the idea of exploring fonts like Fira Code that help with readability and dev aesthetics.</p>
+      </section>
+
+      <section>
+        <h3>Challenges & Growth</h3>
+        <p>Coming from a non-developer background, I struggled a lot at the start of this program - especially with the terminal, Git and Docker. I leaned heavily on online research and class instructions, but I made a commitment to improve with each project. This portfolio is a reflection of that journey. I'm still learning every day, but I'm proud of how far I've come.</p>
+      </section>
+    </div>
+  );
 }
 
 export default Setup;
