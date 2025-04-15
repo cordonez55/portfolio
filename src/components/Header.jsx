e46b656bb3a78783ec69c9bf6b07cbe41de5e4be
cordonez55 <cassy.ordonez@gmail.com>
2025-04-14 20:20:55 -0500

    Update type feature to be aligned with nav bar. Remove from the top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,47 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Typewriter } from 'react-simple-typewriter';
+import { MdEmail, MdPhone } from "react-icons/md";
+import "../App.css";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul style={{ display: "flex", gap: "1rem", listStyle: "none"}}>
-          <li><Link to="/">Home</Link></li>
+    <header className="nav-wrapper">
+      <div className="nav-left">
+        <motion.div
+          className="typing-text"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          >
+            <span className="typing">
+              <Typewriter
+                words={[ 'Learning', 'Growing', 'Building' ]}
+                loop={0}
+                cursor
+                cursorStyle="|"
+                typeSpeed={100}
+                deleteSpeed={60}
+                delaySpeed={1200}
+              />
+            </span>
+          </motion.div>
+      </div>
+
+      <nav className="nav-center">
+        <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
           <li><Link to="/projects">Projects</Link></li>
           <li><Link to="/skills">Skills</Link></li>
           <li><Link to="/resources">Resources</Link></li>
           <li><Link to="/setup">Setup</Link></li>
         </ul>
       </nav>
+
+      <div className="nav-right">
+        <a href="mailto:cassy.ordonez@gmail.com" title="Email"><MdEmail size={22} /></a>
+        <a href="tel:+12042308775" title="Phone"><MdPhone size={21} /></a>
+      </div>
     </header>
   );
 }
